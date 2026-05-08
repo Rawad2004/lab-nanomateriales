f0a75267cf7ce6f6629c6184bfb37ccbe18f152b
Rawad Yecith Muñoz Romero <raw@Rawads-MacBook-Air.local>
2026-05-08 18:01:18 -0500

    feat(reagents): registrar controller y service en el modulo + exportar service

--- backend/src/modules/reagents/reagents.module.ts
+++ backend/src/modules/reagents/reagents.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Reagent } from './entities/Reagent.entity';
+import { ReagentsController } from './reagents.controller';
+import { ReagentsService } from './services/reagents.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Reagent])],
+  controllers: [ReagentsController],
+  providers: [ReagentsService],
 })
 export class ReagentsModule {}
