037de16f7960901bf1ba8a265397e847ff9a98e9
Rawad Yecith Muñoz Romero <raw@Rawads-MacBook-Air.local>
2026-05-08 21:23:17 -0500

    feat(nanomaterials): agregar service con CRUD y toggleActive

--- backend/src/modules/nanomaterials/nanomaterials.module.ts
+++ backend/src/modules/nanomaterials/nanomaterials.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Nanomaterial } from './entity/Nanomaterial.entity';
+import { NanomaterialsService } from './nanomaterials.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Nanomaterial])],
+  providers: [NanomaterialsService],
+  exports: [NanomaterialsService],
 })
 export class NanomaterialsModule {}
