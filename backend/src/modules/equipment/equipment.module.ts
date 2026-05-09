f7e42495d7c82cf44b133a4de96266734210a9ed
Rawad Yecith Muñoz Romero <raw@Rawads-MacBook-Air.local>
2026-05-09 16:36:06 -0500

    feat(equipment): registrar controller y service en el modulo + exportar

--- backend/src/modules/equipment/equipment.module.ts
+++ backend/src/modules/equipment/equipment.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Equipment } from './entity/Equipment.entity';
+import { EquipmentController } from './equipment.controller';
+import { EquipmentService } from './service/equipment.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Equipment])],
+  controllers: [EquipmentController],
+  providers: [EquipmentService],
+  exports: [EquipmentService],
 })
 export class EquipmentModule {}
